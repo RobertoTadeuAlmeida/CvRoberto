326bc046c28061e29efd210a60f85b29e3a478d7
roberto <robertotadeudealmeida@gmail.com>
2024-08-28 13:30:18 -0300

    ajuste nos links de navegação e arquivo js

--- js/main.js
+++ js/main.js
@@ -1,33 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('menu.html')
+
+    const isIndexPage = location.pathname.endsWith("index.html");
+    
+    const menuPath = isIndexPage ? "components/menu.html" : "../components/menu.html";
+    const footerPath = isIndexPage ? "components/footer.html" : "../components/footer.html";
+    
+    fetch(menuPath)
         .then(response => response.text())
         .then(data => {
             document.getElementById("menu-placeholder").innerHTML = data;
         })
         .catch(error => console.error('Erro ao carregar o menu:', error));
-});
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('footer.html')
+    
+    fetch(footerPath)
         .then(response => response.text())
         .then(data => {
             document.getElementById("footer-placeholder").innerHTML = data;
         })
         .catch(error => console.error('Erro ao carregar o footer:', error));
 });
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('pages/menu.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("index-menu-placeholder").innerHTML = data;
-        })
-        .catch(error => console.error('Erro ao carregar o menu:', error));
-});
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('pages/footer.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("index-footer-placeholder").innerHTML = data;
-        })
-        .catch(error => console.error('Erro ao carregar o footer:', error));
-});
-
